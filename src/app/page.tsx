e97bbe5fbb07eafc871e3d94a4ec2fd4f7bf0cca
Manishpande2 <biraalogang@gmail.com>
2025-03-04 08:02:38 +0545

    Add session management and user profile display with NextAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 "use client"
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if(session){
+    return (
+      <>
+        <Image src={session.user?.image as string} alt="profile" width={100} height={100}/>
+        <h1>Hi {session.user?.name}</h1>
+        <p>{session.user?.email}</p>
+
+      </>
+    )
+  }
   return (
     <div>
       <button onClick={() => signIn("google")}>Sign IN With Google</button>
